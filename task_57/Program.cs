962be192bcc9c1cc8cc820e91b3f3ffaaff9d628
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-15 17:49:29 +0300

    Программа на составление частотного словаря двумерного массива (сколько раз встречается элемент массива)

---
+++ task_57/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 57: Составить частотный словарь элементов двумерного массива. 
+Частотный словарь содержит информацию о том, сколько раз встречается элемент 
+входных данных.
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раз
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза
+*/
+
+int[,] FillMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] array = new int[10];
+Console.Write("Введите число строк = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        array[matrix[i, j]] = array[matrix[i, j]] + 1;
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        Console.WriteLine($"Количество {i} = {array[i]}");
+    }
+}
